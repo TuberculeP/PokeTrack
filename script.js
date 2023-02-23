756ae2ae77a1e3ae37b028c7334a85f3355e19d2
Félix Laviéville <lavieville.felix@gmail.com>
2023-02-23 19:27:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const categories = [
+    {
+        name: "Guides de voyage",
+        description: "Itinéraires de voyage à travers différentes régions du monde Pokémon"
+    },
+    {
+        name: "Livres sur les objets et les équipements",
+        description: "Explication des différents types d'objets et d'équipements disponibles dans l'univers de Pokémon"
+    },
+    {
+        name: "Livres sur les types de Pokémon",
+        description: "Présentation des différents types de Pokémon et de leurs forces et faiblesses"
+    },
+    {
+        name: "Livres d'entraînement",
+        description: "Programmes d'entraînement pour les dresseurs de Pokémon"
+    },
+    {
+        name: "Livres sur les légendes et les mythes",
+        description: "Histoires sur les créatures légendaires de l'univers de Pokémon"
+    }
+];
+
+const bookContainer = document.querySelector('.book-container');
+categories.forEach((category) => {
+        const ebook = document.createElement('div');
+        ebook.className = 'ebook';
+        ebook.innerHTML = `
+      <h3>${category.name}</h3>
+      <p>${category.description}</p>
+    `;
+        bookContainer.appendChild(ebook);
+    }
+);
